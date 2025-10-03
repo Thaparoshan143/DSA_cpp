80f981ac6c551051a7594a161bd03a85eea27a40
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-10-03 09:17:01 +0545

    add: basic impl of Queue (Linear, Circular)
    add: basic test setup of both Linear & Circular Queue was carried out, the implmentation is carried such that, LQueue class is for one time use only, CQueue is resuable with FIFO order based override. Currently log(error) information for overflow, underflow are missing. The capacity should be known before hand so, default constructor is deleted

---
+++ impl/stack_queue/queue.cpp
@@ -0,0 +1,173 @@
+#include<includes/Types.hpp>
+
+// basic implementation of Queue (linear/circular, but fixed capacity)
+// Operations
+// 1. Enqueue
+// 2. Dequeue
+// 3. Peek
+// 4. IsEmpty
+// 5. Size
+
+// linear (one time use only) Queue
+template<class T>
+class LQueue
+{
+    public:
+    // default constructor is deleted so that capacity must be known before hand (fixed size so..)
+    LQueue() = delete;
+    LQueue(const uint cap): m_capacity(cap) {   alloc(this, cap);   }
+
+    void Enqueue(const T& d)
+    {
+        // total capacity is full so no enqueue operation..
+        if (m_start + m_count == m_capacity)
+            return;
+
+        this->m_data[m_start + m_count++] = d; 
+    }
+
+    T Dequeue()
+    {
+        // no element to dequeue
+        if (m_start == m_capacity || m_count == 0)
+            return T(-1);
+
+        m_count--;
+        return this->m_data[m_start++];
+    }
+
+    T Peek()
+    {
+        // no element to peek
+        if (m_start == m_capacity || m_count == 0)
+            return T(-1);
+
+        return this->m_data[m_start];
+    }
+
+    bool IsEmpty()
+    {
+        return m_count == 0;
+    }
+
+    uint Size()
+    {
+        return m_count;
+    }
+
+    uint Capacity()
+    {
+        return m_capacity - m_start;
+    }
+
+    void PrintInfo()
+    {
+        std::cout << "Queue Cap: " << Capacity() << " || Element Count: " << Size() << "\n";
+        std::cout << "Data: ";
+        for (int i=0;i<m_count;++i)
+        {
+            std::cout << this->m_data[i+m_start] << ", ";
+        }
+        std::cout << std::endl;
+    }
+
+    private:
+    T *m_data {};
+    uint m_capacity { 1 };
+    uint m_count {};
+    uint m_start {};
+
+    // since only one allocation will be done so simple 
+    static void alloc(LQueue *lq, const uint cap)
+    {
+        lq->m_data = new T[cap];
+    }
+};
+
+// reusable (circular) queue 
+template<class T>
+class CQueue
+{
+    public:
+    // default constructor is deleted so that capacity must be known before hand (fixed size so..)
+    CQueue() = delete;
+    CQueue(const uint cap): m_capacity(cap) {   alloc(this, cap);   }
+
+    void Enqueue(const T& d)
+    {
+        // if queue is already full then overwritten in FIFO order
+        this->m_data[m_start] = d;
+        m_start = (m_start + 1) % m_capacity;
+
+        if (m_count == m_capacity)
+            return;
+
+        m_count++;
+    }
+
+    T Dequeue()
+    {
+        // no element to dequeue
+        if (m_count == 0)
+            return T(-1);
+
+        m_count--;
+        uint prev = m_start;
+        m_start = (m_start + 1) % m_capacity;
+        return this->m_data[prev];
+    }
+
+    T Peek()
+    {
+        // no element to peek
+        if (m_count == 0)
+            return T(-1);
+
+        return this->m_data[m_start];
+    }
+
+    bool IsEmpty()
+    {
+        return m_count == 0;
+    }
+
+    uint Size()
+    {
+        return m_count;
+    }
+
+    uint Capacity()
+    {
+        return m_capacity;
+    }
+
+    // only for the circular queue to know the current position (index) of head
+    uint StartPos()
+    {
+        return m_start;
+    }
+
+    void PrintInfo()
+    {
+        std::cout << "Queue Cap: " << Capacity() << " || Element Count: " << Size() << "\n";
+        std::cout << "Data: ";
+        for (int i=0;i<m_count;++i)
+        {
+            // std::cout << "Index: " << (i + m_start) % m_capacity << std::endl;
+            std::cout << this->m_data[(i + m_start + (m_capacity - m_count)) % m_capacity] << ", ";
+        }
+        std::cout << std::endl;
+    }
+
+    private:
+    T *m_data {};
+    uint m_capacity { 1 };
+    uint m_count {};
+    uint m_start {};
+
+    // here simple allocation as well
+    static void alloc(CQueue *cq, const uint cap)
+    {
+        cq->m_data = new T[cap];
+    }
+};
--- main.cpp
+++ main.cpp
@@ -4,17 +4,17 @@
 #include<utils/Log.cpp>
 #include<utils/Generators.cpp>
 #include<utils/Utils.cpp>
-#include<stack_queue/stack.cpp>
+#include<stack_queue/queue.cpp>
 
 using namespace Utils;
 
 int main()
 {
-    Stack<int> temp {};
+    CQueue<int> temp(4);
 
-    for (int i=0;i<5;++i)
+    for (int i=0;i<7;++i)
     {
-        temp.Push(i+1);
+        temp.Enqueue(i+1);
     }
 
     temp.PrintInfo();
@@ -22,37 +22,38 @@ int main()
     Seperator();
     for (int i=0;i<2;++i)
     {
-        std::cout << "Popped: " << temp.Pop() << std::endl;
+        std::cout << "Dequeue: " << temp.Dequeue() << std::endl;
     }
     
     Seperator();
-    Log("Stack Peek: ", temp.Peek());
-    Log("Stack Peek (check again): ", temp.Peek());
-    Log("Is Stack Empty: (1/YES || 0/NO)", temp.IsEmpty());
+    Log("Queue Peek: ", temp.Peek());
+    Log("Queue Peek (check again): ", temp.Peek());
+    Log("Is Queue Empty: (1/YES || 0/NO)", temp.IsEmpty());
     
     Seperator("*");
     temp.PrintInfo();
     Seperator("*");
 
-    // overflowing the Pop than available as test case (can it handle?)
-    temp.Pop();
-    temp.Pop();
-    temp.Pop();
+    // overflowing the Dequeue than available as test case (can it handle?)
+    temp.Dequeue();
+    temp.Dequeue();
+    temp.Dequeue();
 
-    Log("Overflow (outbound) Pop check: ", temp.Pop());
+    Log("Overflow (outbound) Pop check: ", temp.Dequeue());
     Log("Overflow (outbound) Peek check: ", temp.Peek());
-    Log("Stack After Empty, check Cap: ", temp.Capacity());
-    Log("Stack After Empty, check Element count: ", temp.Size());
-    Log("Stack After Empty, check IsEmpty: ", temp.IsEmpty());
+    Log("Queue After Empty, check Cap: ", temp.Capacity());
+    Log("Queue After Empty, check Element count: ", temp.Size());
+    Log("Queue After Empty, check IsEmpty: ", temp.IsEmpty());
     
-    // Empty Stack info
+    // Empty Queue info
     Seperator("#");
     temp.PrintInfo();
     Seperator("#");
-    // Reusing Stack for check
+
+    // Reusing Queue for check? LQueue is generally one time use only and CQueue is reusable..
     for (int i=0;i<3;++i)
     {
-        temp.Push(i+1);
+        temp.Enqueue(i+10);
     }
     
     temp.PrintInfo();

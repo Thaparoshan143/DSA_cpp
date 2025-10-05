300ebbfdf67eae4f5169e47cce07da73da23cb2f
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-10-05 10:24:03 +0545

    add: LogMsg function with LogLevel
    Now, we can simply write different log message with its level from, info, message, warning and error with some prefix

--- utils/Log.cpp
+++ utils/Log.cpp
@@ -8,6 +8,14 @@
 #define END_LINE_CMD std::endl // cpp style (buffer flushed)
 // #define END_LINE_CMD "\n" // c style
 
+// static log prefix for different logtype
+static String s_log_prefix[] = {
+    "=> INFO: ",
+    "$ MESSAGE: ",
+    "# WARNING: ",
+    "** ERROR: "
+};
+
 namespace Utils
 {
     template<typename T = uint>
@@ -39,6 +47,19 @@ namespace Utils
             std::cout << item << vecSep;
         }
         std::cout << END_LINE_CMD;
+    }
+
+    enum LogLevel
+    {
+        INFO = 0,
+        MESSAGE = 1, // info and message same?
+        WARNING = 2,
+        ERROR = 3
+    };
+
+    void LogMsg(const String& msg, const LogLevel& ll)
+    {
+        std::cout << END_LINE_CMD << s_log_prefix[ll] << msg << END_LINE_CMD << std::endl;
     }
 }
 

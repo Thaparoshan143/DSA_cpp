e8a062ca60ab724c13a2d37d38ca81830ec90700
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-10-11 12:24:13 +0545

    add: basic binary tree (BNode class), three traversal impl
    Using the Node basis or linked list basis binary tree basic impl was tested in main source, along with traversals (namely pre, in & post order.

---
+++ impl/tree/binary.cpp
@@ -0,0 +1,67 @@
+#include<includes/Types.hpp>
+
+
+// BNode (Binary Node) aggregate..
+template<typename T>
+struct BNode
+{
+    T value {};
+    BNode *left {};
+    BNode *right {};
+};
+
+template<typename T>
+using BNodePtr = BNode<T>*;
+
+template<typename T>
+using BNodeRef = BNode<T>&;
+
+template<typename T>
+BNodePtr<T> GetNewBNode(const T& val)
+{
+    BNodePtr<T> temp = new BNode<T>;
+    temp->value = val;
+    return temp;
+}
+
+/* 
+There are three traversal methods:
+1. Pre-order
+2. In-ordedr
+3. Post-order
+
+basic implementation is as:
+*/
+
+template<typename T>
+void pre_order_traversal(const BNodePtr<T> root)
+{
+    if (root == nullptr)
+        return;
+
+    std::cout << root->value << ", ";
+    pre_order_traversal(root->left);
+    pre_order_traversal(root->right);
+}
+
+template<typename T>
+void in_order_traversal(const BNodePtr<T> root)
+{
+    if (root == nullptr)
+        return;
+    
+    in_order_traversal(root->left);
+    std::cout << root->value << ", ";
+    in_order_traversal(root->right);
+}
+
+template<typename T>
+void post_order_traversal(const BNodePtr<T> root)
+{
+    if (root == nullptr)
+        return;
+
+    post_order_traversal(root->left);
+    post_order_traversal(root->right);
+    std::cout << root->value << ", ";
+}
--- main.cpp
+++ main.cpp
@@ -4,88 +4,37 @@
 #include<utils/Log.cpp>
 #include<utils/Generators.cpp>
 #include<utils/Utils.cpp>
-#include<hash/table.cpp>
+#include<tree/binary.cpp>
 
 using namespace Utils;
 
-template<typename T>
-struct Transform
-{
-    struct Point3D {    T x {}, y {}, z {}; };
-
-    Point3D position {}, rotation {}, scale {};
-};
-
-// temporary object to store in hash table.. hash is done by its id, .. (can also be from name)
-struct Object
-{
-    uint id {};
-    String name { "Default" };
-    Transform<float> transform {};
-
-    // simple hash overload with id based..
-    // int operator%(uint rhs) const
-    // {
-    //     return this->id % rhs;
-    // }
-
-    // another hash overload fn (taking name and adding all the char ASCII value then finally modulo)
-    int operator%(uint rhs) const
-    {
-        // logic to create the object hash from its identity
-        uint64_t totalChar {};
-        for (const auto& item : name)
-        {
-            totalChar += static_cast<uint64_t>(item);
-        }
-        return totalChar % rhs;
-    }
-};
-
-void print_obj(const Object& o)
-{
-    Utils::Seperator();
-    std::cout << "# Object Info:" << std::endl;
-    std::cout << "Id: " << o.id << "\nName: " << o.name << std::endl;
-    std::cout << "Position: " << o.transform.position.x << ", " << o.transform.position.y << ", " << o.transform.position.z << "\n";
-    std::cout << "Rotation: " << o.transform.rotation.x << ", " << o.transform.rotation.y << ", " << o.transform.rotation.z << "\n";
-    std::cout << "Scale: " << o.transform.scale.x << ", " << o.transform.scale.y << ", " << o.transform.scale.z << "\n";
-    Utils::Seperator();
-}
-
 int main()
 {
-    HashTableProps<Object> htp {};
-    htp.tableSize = TWO_POW(5);
-    htp.hashFn = hash_func_mod;
-    HashTable<Object> ht(htp);
+    BNodePtr<int> root { GetNewBNode(100) };
+    BNodePtr<int> node {};
 
-    Object obj1 {}, obj2 {};
-    obj2.id = 150;
-    obj2.name = "Second";
-    obj2.transform.scale.z = 5;
-    print_obj(obj1);
-    print_obj(obj2);
+    node = GetNewBNode<int>(10);
+    root->left = node;
+    node = GetNewBNode<int>(20);
+    root->right = node;
+    node = GetNewBNode<int>(50);
+    root->right->right = node;
 
-    auto obj1Hash = ht.Place(obj1);
-    auto obj2Hash = ht.Place(obj2);
 
-    std::cout << "Object 1 Hash: " << obj1Hash << std::endl;
-    std::cout << "Object 2 Hash: " << obj2Hash << std::endl;
+    Utils::LogMsg("Different Traversals", LogLevel::INFO);
+    Utils::LogMsg("Pre-Order", LogLevel::MESSAGE);
+    pre_order_traversal(root);
+    std::cout << std::endl;
 
-    auto objret = ht.Retrieve(obj1Hash);
-    if (objret)
-        print_obj(*objret);
+    Utils::LogMsg("In-Order", LogLevel::MESSAGE);
+    in_order_traversal(root);
+    std::cout << std::endl;
 
-    objret = ht.Retrieve(obj2Hash);
-    if (objret)
-        print_obj(*objret);
+    Utils::LogMsg("Post-Order", LogLevel::MESSAGE);
+    post_order_traversal(root);
 
-    ht.Remove(obj1Hash);
+    std::cout << std::endl;
+    Seperator();
 
-    objret = ht.Retrieve(obj1Hash);
-    if (objret)
-        print_obj(*objret);
-    
     return 0;
 }
